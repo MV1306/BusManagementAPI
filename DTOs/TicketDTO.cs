bed11dab9327979bb73509c69567527fc0b76ec8
Manoj Prabakaran <manojprabakaran2001@gmail.com>
2025-06-09 22:03:11 +0530

    Ticket Request Changes

--- DTOs/TicketDTO.cs
+++ DTOs/TicketDTO.cs
@@ -17,4 +17,17 @@
         public DateTime BookingDate { get; set; }
         public DateTime? RedeemedDate { get; set; }
     }
+
+    public class TicketRequestDTO
+    {
+        public string RouteCode { get; set; }
+        public string FromStage { get; set; }
+        public string ToStage { get; set; }
+        public string BusType { get; set; }
+        public int StagesTravelled { get; set; }
+        public decimal Fare { get; set; }
+        public string UserName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+    }
 }
