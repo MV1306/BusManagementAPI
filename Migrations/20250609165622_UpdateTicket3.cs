c01a38130906b565b66fc674ba271c35798bfd0d
Manoj Prabakaran <manojprabakaran2001@gmail.com>
2025-06-09 22:26:48 +0530

    Ticket Request Migration Changes

---
+++ Migrations/20250609165622_UpdateTicket3.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTicket3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Fare",
+                table: "Tickets",
+                newName: "TotalFare");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BaseFare",
+                table: "Tickets",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Passengers",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BaseFare",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Passengers",
+                table: "Tickets");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalFare",
+                table: "Tickets",
+                newName: "Fare");
+        }
+    }
+}
