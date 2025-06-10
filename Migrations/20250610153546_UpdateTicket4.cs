7a73f1093767a14e29e19b0974e9157135299fce
Manoj Prabakaran <manojprabakaran2001@gmail.com>
2025-06-10 21:17:27 +0530

    Ticket Controller Migration Changes

---
+++ Migrations/20250610153546_UpdateTicket4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTicket4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RouteID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BaseFare = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BusType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FromStage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsRedeemed = table.Column<bool>(type: "bit", nullable: false),
+                    MobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Passengers = table.Column<int>(type: "int", nullable: false),
+                    RedeemedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    StagesTravelled = table.Column<int>(type: "int", nullable: false),
+                    ToStage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalFare = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketID);
+                    table.ForeignKey(
+                        name: "FK_Tickets_BusRoutes_RouteID",
+                        column: x => x.RouteID,
+                        principalTable: "BusRoutes",
+                        principalColumn: "RouteID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_RouteID",
+                table: "Tickets",
+                column: "RouteID");
+        }
+    }
+}
---
+++ Migrations/20250610154128_UpdateTicket7.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTicket7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookingRefID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RouteID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FromStage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToStage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BusType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StagesTravelled = table.Column<int>(type: "int", nullable: false),
+                    BaseFare = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Passengers = table.Column<int>(type: "int", nullable: false),
+                    TotalFare = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsRedeemed = table.Column<bool>(type: "bit", nullable: false),
+                    IsCancelled = table.Column<bool>(type: "bit", nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RedeemedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketID);
+                    table.ForeignKey(
+                        name: "FK_Tickets_BusRoutes_RouteID",
+                        column: x => x.RouteID,
+                        principalTable: "BusRoutes",
+                        principalColumn: "RouteID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_RouteID",
+                table: "Tickets",
+                column: "RouteID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
