88e4081be71a20111490293ea4d3c1e1c1e3606c
Manoj Prabakaran <manojprabakaran2001@gmail.com>
2025-06-10 22:11:19 +0530

    Get Tickets Changes

--- DTOs/TicketDTO.cs
+++ DTOs/TicketDTO.cs
@@ -3,12 +3,15 @@
     public class TicketResponseDTO
     {
         public Guid TicketID { get; set; }
+        public string BookingRefId {  get; set; }
         public string RouteCode { get; set; }
         public string FromStage { get; set; }
         public string ToStage { get; set; }
         public string BusType { get; set; }
         public int StagesTravelled { get; set; }
         public decimal Fare { get; set; }
+        public int Passengers {  get; set; }
+        public decimal TotalFare { get; set; }
         public string UserName { get; set; }
         public string MobileNo { get; set; }
         public string Email { get; set; }
